a82bc608246b4417e41db3466fc6e1d7f535353f
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2026-04-12 22:29:00 -0500

    Add items hooks

---
+++ src/hooks/useItems.js
@@ -0,0 +1,62 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { getItemById, addItem, getItems, updateItem, deleteItemById } from "../services/itemService";
+
+export function useItems() {
+    return useQuery({
+        queryKey: ["items"], 
+        queryFn: getItems,
+    })
+}
+
+
+export function useItemById(id) {
+    return useQuery({
+        queryKey: ["items", id], 
+        queryFn: () => getItemById(id), 
+        enabled: !!id,
+    })
+}
+
+
+export function useAddItem() {
+    const qc = useQueryClient()
+    return useMutation({
+        mutationFn: addItem, 
+        onSuccess: () => {
+            qc.invalidateQueries({queryKey: ["items"]})
+        }, 
+        onError: (error) => {
+            console.log("Error while adding an item", error.message)
+        }
+    })
+}
+
+export function useUpdateItem(){
+    const qc = useQueryClient()
+    return useMutation({
+        mutationFn: ({id, data}) => updateItem(id, data), 
+        onSuccess: () => {
+            qc.invalidateQueries({
+                queryKey: ["items"],
+            })
+        },
+        onError: (error) => {
+            console.log("Error while updating an item", error.message)
+        }
+    })
+}
+
+export function useDeleteItem(id) {
+    const qc = useQueryClient()
+    return useMutation({
+        mutationFn: deleteItemById, 
+        onSuccess: () => {
+            qc.invalidateQueries({
+                queryKey: ["items"],
+            })
+        }, 
+        onError: (error) => {
+            console.log("Error while deleting an item", error.message)
+        }
+    })
+}
--- src/services/outfitService.js
+++ src/services/outfitService.js
@@ -2,11 +2,11 @@ import api from "./api";
 
 export const getOutfits = () => api.get("/outfits").then(r => r.data)
 
-export const getOutfitById = (id) => api.get(`/outfit/${id}`).then(r => r.data)
+export const getOutfitById = (id) => api.get(`/outfits/${id}`).then(r => r.data)
 
 export const addOutfit = (data) => api.post("/outfits", data).then(r => r.data)
 
 export const updateOutfit = (id, data) => api.put(`/outfits/${id}`, data).then(r => r.data)
 
-export const deleteById = (id) => api.delete(`/outfits/${id}`).then(r => r.data)
+export const deleteOutfitById = (id) => api.delete(`/outfits/${id}`).then(r => r.data)
 
