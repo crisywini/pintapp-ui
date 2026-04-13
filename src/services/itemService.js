2529d095252f7deee1a2760318756fa6289b7955
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2026-04-12 22:03:53 -0500

    Add items api calls

---
+++ src/services/itemService.js
@@ -0,0 +1,12 @@
+import api from "./api";
+
+
+export const getItems = () => api.get("/items").then(r => r.data)
+
+export const addItem = (data) => api.post("/items", data).then(r => r.data)
+
+export const getItemById = (id) => api.get(`/items/${id}`).then(r => r.data)
+
+export const deleteItemById = (id) => api.delete(`/items/${id}`).then(r => r.data)
+
+export const updateItem = (id, data) => api.put(`/items/${id}`, data).then(r => r.data)
