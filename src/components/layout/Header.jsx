4754831d157c9102482c8010470659a3adea47c7
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2026-04-13 22:27:41 -0500

    Add some cool things in the header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,13 +1,38 @@
 import { Link } from "react-router-dom";
-
+import './Header.css'
+import { useState } from "react";
 
 function Header() {
+
+    const [searchOpen, setSearchOpen] = useState(false)
+
     return (
-        <header>
-            <h1>Pinta</h1>
-            <nav>
-                <Link to="/Home"></Link>
+        <header className="header">
+
+            <nav className="header_nav">
+                <ul className="header_links">
+                    <li>Sport</li>
+                    <li>Casual</li>
+                    <li>Elegant</li>
+                    <li>Italian</li>
+                </ul>
+
             </nav>
+            <div className="title">
+                <h1>Pinta</h1>
+            </div>
+
+            <div className="search">
+                <button onClick={() => setSearchOpen(!searchOpen)}>
+                    Search
+                </button>
+
+                { searchOpen && (
+                    <div className="search_panel">
+                        <p>Results</p>
+                    </div>
+                )}
+            </div>
         </header>
     )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,27 +3,9 @@ import ItemCard from "../../components/items/ItemCard";
 
 function Home(){
 
-    const {data: items, isLoading, isError, error} = useItems()
-    console.log("Items.......")
-    console.log(items)
-    if (isLoading) return <p>Loading...</p>
-    if (isError) return <p>Found and error {error.message}</p>
-
     return (
         <div>
-            <h1>Items</h1>
-            {items.map( item => (
-                <ItemCard
-                    key={item.id}
-                    name={item.name}
-                    category={item.category}
-                    brand={item.brand}
-                    photo={item.photo}
 
-                />
-            ))
-
-            }
         </div>
     )
 
