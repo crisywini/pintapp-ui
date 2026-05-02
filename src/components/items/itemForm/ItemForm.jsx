cfda35b9db8df16ab27f44bab2baddb779f196fa
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2026-05-02 15:07:21 -0500

    Add item component

---
+++ src/components/items/itemForm/ItemForm.jsx
@@ -0,0 +1,3 @@
+import './ItemForm.css'
+
+
---
+++ src/components/items/itemForm/steps/01_SaveImageForm.jsx
@@ -0,0 +1,27 @@
+
+
+const OPENING_LINES = ["Every story begins with one piece.", "The outfit hasn't met this piece yet.",
+     "Something brought you here. What is it?", "Every look has a first chapter. This is yours.",
+      "Before the outfit, there was the item.", "One piece. One memory. One story.", "The best wardrobes are built one piece at a time.", 
+     "This piece has been waiting to be remembered.", "What are we adding to the story today?", "Every great look starts somewhere. Start here.", 
+      "The closet remembers everything you add to it.", "Is this a new piece or an old love?", "Colombia called. It wants to know what you're adding.", 
+    "Manolo started with one shoe. You start here.", "What's the story behind this one?"]
+
+const line = OPENING_LINES[Math.floor(Math.random() * OPENING_LINES.length)]
+
+
+
+function SaveImageForm({data, onChange}) {
+
+    return (
+        <div className="imageContainer">
+            <h2>{line}</h2>
+            <input type="image" value={data.image}>
+            </input>
+
+        </div>
+    )
+
+}
+
+export default SaveImageForm
